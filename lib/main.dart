531fda9a16d70a2eaac3183135e661ffc720988f
Will Russell <will@will-russell.com>
2020-05-01 22:46:02 +0100

    Add flashing for last 20 seconds

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,9 @@ class _MyHomePageState extends State<MyHomePage> {
   int _secondsSinceEpoch = DateTime.now().toUtc().millisecondsSinceEpoch;
   CountdownTimer countdownTimer;
   String _countdownText = "0:00";
+  Color backgroundColour = Colors.white;
+  Color textColour = Colors.black;
+
 
   void _startCountdown(var length) {
 
@@ -61,14 +64,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
     sub.onData((duration) {
       setState(() {
-
         int minutes = duration.remaining.inMinutes;
         String seconds = (duration.remaining.inSeconds % 60).toString().padLeft(2, '0');
         _countdownText = "$minutes:$seconds"; 
+
+        if (duration.remaining.inSeconds < 20 && duration.remaining.inSeconds % 2 == 1) {
+          backgroundColour = Colors.red;
+          textColour = Colors.white; 
+        } else {
+          backgroundColour = Colors.white;
+          textColour = Colors.black; 
+        }
+
       });
 
       sub.onDone(() {
         _state = false;
+        backgroundColour = Colors.white;
+        textColour = Colors.black;
         sub.cancel();
       });
     }); 
@@ -88,45 +101,52 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Time Remaining:',
-            ),
-            StreamBuilder(
-              stream: database.onValue,
-              builder: (context, snap) {
-                if (snap.hasData && !snap.hasError && snap.data.snapshot.value != null) {
-                  Map data = snap.data.snapshot.value;
-                  _state = data['start'];
-                  _countdownTime = data['time'];
-                  _secondsSinceEpoch = data['timestamp'];
-                  if (_state == false) {
-                    _stopCountdown();
-                    _countdownText = "$_countdownTime:00";
-                  } else if (_state == true) {
-                    var now = DateTime.now().toUtc().millisecondsSinceEpoch;
-                    _secondsSinceEpoch = DateTime.now().add(Duration(minutes: _countdownTime)).millisecondsSinceEpoch;
-                    var diff =  _secondsSinceEpoch - now;
-                    _startCountdown(diff);
+      body: Container(
+        decoration: new BoxDecoration(color: backgroundColour),
+        child: new Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Time Remaining:',
+                style: new TextStyle(
+                  color: textColour
+                )
+              ),
+              StreamBuilder(
+                stream: database.onValue,
+                builder: (context, snap) {
+                  if (snap.hasData && !snap.hasError && snap.data.snapshot.value != null) {
+                    Map data = snap.data.snapshot.value;
+                    _state = data['start'];
+                    _countdownTime = data['time'];
+                    _secondsSinceEpoch = data['timestamp'];
+                    if (_state == false) {
+                      _stopCountdown();
+                      _countdownText = "$_countdownTime:00";
+                    } else if (_state == true) {
+                      var now = DateTime.now().toUtc().millisecondsSinceEpoch;
+                      _secondsSinceEpoch = DateTime.now().add(Duration(minutes: _countdownTime)).millisecondsSinceEpoch;
+                      var diff =  _secondsSinceEpoch - now;
+                      _startCountdown(diff);
+                    }
+                    return Text("$_countdownText",
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: new TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 150,
+                          color: textColour)
+                    );
                   }
-                  return Text("$_countdownText",
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis,
-                    style: new TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 150),
-                  );
-                }
-                else
-                  return Text("Error: No Time");
-              },
-            ),
-          ],
+                  else
+                    return Text("Error: No Time");
+                },
+              ),
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
